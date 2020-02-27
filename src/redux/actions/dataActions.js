da7da7d90cb2f366323773d0285d143971174005
Dan Lenington <danlening02@gmail.com>
2020-02-27 15:07:33 -0600

    Redux setup
    Add to dataActions file for Redux

---
+++ src/redux/actions/dataActions.js
@@ -0,0 +1,34 @@
+import {
+  SET_POSTS,
+  LOADING_DATA,
+  LIKE_POST,
+  UNLIKE_POST,
+  DELETE_POST,
+  SET_ERRORS,
+  CLEAR_ERRORS,
+  LOADING_UI,
+  SET_POST,
+  STOP_LOADING_UI,
+  SUBMIT_COMMENT
+} from "../types";
+import axios from "axios";
+
+//Get all posts
+export const getPosts = () => dispatch => {
+  dispatch({ type: LOADING_DATA });
+  axios
+    .get("/posts")
+    .then(res => {
+      dispatch({
+        type: SET_POSTS,
+        payload: res.data
+      });
+    })
+    .catch(err => {
+      //clear all posts
+      dispatch({
+        type: SET_POSTS,
+        payload: []
+      });
+    });
+};
